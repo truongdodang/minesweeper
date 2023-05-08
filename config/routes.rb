0cedc240691d2579631235d4a8aeeaa81e6d24aa
Truong Do <truongdodang@gmail.com>
2023-05-08 17:02:51 +0700

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/home", to: "home#index"
   resources :boards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
